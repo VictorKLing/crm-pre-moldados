2591f9226a8f10de5b5bc7b9f1fd00258302b90a
VICTOR KLING PEREIRA <victor_pereira150@estudante.sesisenai.org.br>
2025-11-24 20:52:34 -0300

    add home

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,8 +2,8 @@ export default function Dashboard() {
     return (
       <div style={{ padding: 20 }}>
         <h1>Dashboard</h1>
-        <p>Você está logado. 🎉</p>
-        <a href="/dashboard/clientes">Ver Clientes</a>
+        <p>Você está logado. 🎉</p> 
+        <a className="block w-max mt-4 py-2.5 px-4 bg-blue-600 text-white rounded-md font-bold" href="/dashboard/clientes">Ver Clientes</a>
       </div>
     );
   }
